b3a6be86fc05059cf431eac629bb2e5c2529818a
Alex Lin <welin@welin.group.on>
2017-02-13 18:56:57 -0800

    add in download vundle inside setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
-ln -s `pwd`/.gitconfig ~/.gitconfig
+# unlink symlinks
+unlink ~/.gitconfig
+unlink ~/.vim
+unlink ~/.vimrc
 
+# clone vundle for vim plugin managament
+git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
+
+# symlink for gitconfig, vimrc and vim
+ln -s `pwd`/.gitconfig ~/.gitconfig
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.vim ~/.vim
+
+install plugins
 vim +PluginInstall +qall
 
